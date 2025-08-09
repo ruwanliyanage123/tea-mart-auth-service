c74cccbf21d86461ae2b53666187ae09d8382741
Ruwan <ruwanliyanagegalle1994@gmail.com>
2025-08-09 08:27:58 +0530

    first commit

---
+++ src/main/java/com/tea/mart/tea_mart_auth_service/TeaMartAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tea.mart.tea_mart_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeaMartAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeaMartAuthServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tea/mart/tea_mart_auth_service/TeaMartAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tea.mart.tea_mart_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeaMartAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
